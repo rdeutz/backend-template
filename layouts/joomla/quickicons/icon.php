9ec71738bd1ac18ff39672c081422bb3afcd1909
coolcat-creations <mail@coolcat-creations.com>
2019-05-09 13:05:58 +0200

    Regular Button size in Quick Icons

--- layouts/joomla/quickicons/icon.php
+++ layouts/joomla/quickicons/icon.php
@@ -58,7 +58,7 @@ if ($id !== '')
 	</a>
 	<?php // Add the link to the edit-form
 		if (!empty($displayData['linkadd'])): ?>
-			<a class="btn btn-sm btn-secondary btn-block quickicon-linkadd j-links-link mt-2" href="<?php echo $displayData['linkadd']; ?>">
+			<a class="btn btn-secondary btn-block quickicon-linkadd j-links-link mt-2" href="<?php echo $displayData['linkadd']; ?>">
 				<span class="fa fa-plus mr-2" aria-hidden="true"></span>
 				<span class="sr-only"><?php echo Text::sprintf('MOD_QUICKICON_ADD_NEW', $add); ?></span>
 				<span aria-hidden="true"><?php echo $add; ?></span>
